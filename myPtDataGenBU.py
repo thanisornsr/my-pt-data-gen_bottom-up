72771ab3b1e914ed988c43ca27997c6babb25c87
thaniosornsr <thanisornsr@gmail.com>
2021-04-09 16:59:46 +0200

    Update myPtDataGenBU.py

--- myPtDataGenBU.py
+++ myPtDataGenBU.py
@@ -41,9 +41,14 @@ class Pt_datagen_bu:
 
 		self.get_data_from_dir()
 		self.get_pair_dict()
-		# split kps and valids
-		# get wh dict
-		# 
+		self.start_idx, self.end_idx, self.n_batchs = self.get_start_end_idx()
+		self.split_kp_and_v()
+
+		self.limb_list = [(0,1),(2,0),(0,3),(0,4),(3,5),(4,6),(5,7),(6,8),(4,3),(3,9),(4,10),(10,9),(9,11),(10,12),(11,13),(12,14)]
+		self.n_keypoints = 15
+		self.n_limbs = 16
+		print('Create datagen_{}: Done ...'.format(self.data_for))
+
 
 	def get_data_from_dir(self):
 		temp_image_id_with_label = []
@@ -73,11 +78,11 @@ class Pt_datagen_bu:
 			temp_vid_to_id_dict[anno_file] = current_image_id_with_label
 
 			
-
+			current_track_id = []
+			current_image_id = []
+			current_kpv = []
 			for anno in data_annotations:
-				current_track_id = []
-				current_image_id = []
-				current_kpv = []
+				
 				temp_keys = list(anno.keys())
 				to_check_keys = ['image_id','bbox','track_id','keypoints']
 				if all(item in temp_keys for item in to_check_keys):
@@ -91,22 +96,19 @@ class Pt_datagen_bu:
 								current_kpv.append(anno['keypoints'])
 
 				# create dict
-				current_unique_image_id = list(set(current_image_id))
-				for cid in current_unique_image_id:
-					cidx = [ x for x in range(len(current_image_id)) if current_image_id[x] == cid]
-					c_track_id = [current_track_id[x] for x in cidx]
-					c_kpv = [current_kpv[x] for x in cidx]
 
-					temp_id_to_track_id[cid] = c_track_id
-					temp_id_to_kpv[cid] = c_kpv
+			current_unique_image_id = list(set(current_image_id))
+			for cid in current_unique_image_id:
+				cidx = [ x for x in range(len(current_image_id)) if current_image_id[x] == cid]
+				c_track_id = [current_track_id[x] for x in cidx]
+				c_kpv = [current_kpv[x] for x in cidx]
+
+				temp_id_to_track_id[cid] = c_track_id
+				temp_id_to_kpv[cid] = c_kpv
 
 
 		temp_id_to_file_dict = {temp_image_id_with_label[i]:temp_file_name_with_label[i] for i in range(len(temp_image_id_with_label))}
 
-		# self.bbox = temp_anno_bbox_list
-		# self.kps_and_valid = temp_anno_kp_list
-		# self.track_ids = temp_anno_track_id_list
-		# self.img_ids = temp_anno_id_list
 		self.id_to_file_dict = temp_id_to_file_dict
 		self.vid_to_id_dict = temp_vid_to_id_dict
 		self.id_to_track_id = temp_id_to_track_id
@@ -136,4 +138,47 @@ class Pt_datagen_bu:
 		self.id_to_wh = temp_wh_dict
 		self.img_ids = temp_img_ids
 		self.n_imgs = len(temp_img_ids)
+
+	def get_start_end_idx(self):
+		max_idx = self.n_imgs
+		temp_batch_size = self.batch_size
+		l = list(range(max_idx))
+		temp_start_idx = l[0::temp_batch_size]
+		def add_batch_size(num,max_id=max_idx,bz=temp_batch_size):
+			return min(num+bz,max_id)
+		temp_end_idx = list(map(add_batch_size,temp_start_idx))
+		temp_n_batchs = len(temp_start_idx)
+
+		return temp_start_idx, temp_end_idx, temp_n_batchs
+
+	def get_target_valid_joint(self,input_kav):
+		splited_kps = []
+		splited_valids = []
+		for temp_anno_kp in input_kav:
+			temp_x = np.array(temp_anno_kp[0::3])
+			temp_y = np.array(temp_anno_kp[1::3])
+			temp_valid = np.array(temp_anno_kp[2::3])
+			temp_valid = temp_valid > 0
+			temp_valid = temp_valid.astype('float32')
+			temp_target_coord = np.stack([temp_x,temp_y],axis=1)
+			temp_target_coord = temp_target_coord.astype('float32')
+
+			splited_kps.append(temp_target_coord)
+			splited_valids.append(temp_valid)
+
+		return splited_kps,splited_valids
+
+	def split_kp_and_v(self):
+		temp_id_to_kp = {}
+		temp_id_to_valid = {}
+		temp_id_to_kpv = self.id_to_kpv
+		for i_id,kpv in temp_id_to_kpv.items():
+			t_ks,t_vs = self.get_target_valid_joint(kpv)
+			temp_id_to_kp[i_id] = t_ks
+			temp_id_to_valid[i_id] = t_vs
+
+		self.id_to_kp = temp_id_to_kp
+		self.id_to_valid = temp_id_to_valid
+
+
 
